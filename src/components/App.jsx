2c46635bb95a483728318a0c25cbb2098f77d9da
Arturo <atercerosb@univalle.edu>
2021-11-15 13:08:03 -0400

    maquetacion login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
-
+import React from 'react'
+import { LoginUI } from './LoginUI'
 import './styles/App.css'
 
 function App() {
- 
+
 
   return (
     <div className="App">
- 
+      <LoginUI/>
     </div>
   )
 }
---
+++ src/components/LoginUI.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Login } from "./Login"
+
+function LoginUI() {
+    return (
+        <div className="login">
+            <img src="https://svgshare.com/i/c09.svg" alt="logo" />
+            <Login />
+        </div>
+    )
+}
+
+export { LoginUI }
